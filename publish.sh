ca4a468a844408696e2c24676b1b78ffbdaa7dee
Ricardo <rikardu90@gmail.com>
2026-02-13 05:42:16 +0100

    fix: base href

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 dotnet clean
 dotnet publish -c Release
 
+# Remove old docs folder
 rm -rf docs
 mkdir docs
+
+# Copy the contents of wwwroot into docs
 cp -r bin/Release/net8.0/publish/wwwroot/* docs/
+
+# Optional: GitHub Pages custom 404
 cp docs/index.html docs/404.html
