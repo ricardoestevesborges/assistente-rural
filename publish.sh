f810867c2c27b7175f02f96762fd9d4a3fe043a6
Ricardo <rikardu90@gmail.com>
2026-02-15 17:01:20 +0100

    fix: baseref

--- publish.sh
+++ publish.sh
@@ -16,7 +16,4 @@ touch docs/.nojekyll
 # Fix: GitHub Pages 404 redirect for Blazor routing
 cp docs/index.html docs/404.html
 
-# Optional: If you want to automate the base href change during build
-sed -i 's/<base href="\/" \/>/<base href="\/assistente-rural\/" \/>/g' docs/index.html
-
 echo "Build complete. Remember to push the 'docs' folder to GitHub."
