1e835d1115a5b67103c2586cf0ded22ab53e03b2
Ricardo <rikardu90@gmail.com>
2026-02-15 16:59:50 +0100

    fix: nojekyll

--- publish.sh
+++ publish.sh
@@ -10,6 +10,9 @@ mkdir docs
 # Copy the contents of the publish wwwroot into docs
 cp -r bin/Release/net8.0/publish/wwwroot/* docs/
 
+# Add this line to tell GitHub not to ignore Blazor files
+touch docs/.nojekyll
+
 # Fix: GitHub Pages 404 redirect for Blazor routing
 cp docs/index.html docs/404.html
 
